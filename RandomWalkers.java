a25a2bedab43378f8c9eeecc66393a8248b7db9e
proger <89520927+TheProgerOne@users.noreply.github.com>
2022-04-15 09:37:05 +0500

    Update RandomWalkers.java

--- RandomWalkers.java
+++ RandomWalkers.java
@@ -7,7 +7,7 @@ import java.lang.Math;
 public class RandomWalkers {
     public static void main(String[] args) {
         int r = Integer.parseInt(args[0]);
-        double trials =  Double.parseInt(args[1]);
+        double trials =Double.parseDouble(args[1]);
         double random;
         double sum=0;
         for (int t = 0; t < trials; t++) {
@@ -15,22 +15,22 @@ public class RandomWalkers {
             int y = 0;
             int steps = 0;
             int md = x + y;
-        while (md != r){
-            random=Math.random();
-            // right
-            if (random < 0.25) x++;
-            // left
-            else if (random < 0.5) x--;
-            // up
-            else if (random < 0.75) y++;
-            // down
-            else if (random < 1.00) y--;
+            while (md != r){
+                random=Math.random();
+                // right
+                if (random < 0.25) x++;
+                    // left
+                else if (random < 0.5) x--;
+                    // up
+                else if (random < 0.75) y++;
+                    // down
+                else if (random < 1.00) y--;
 
-            md = Math.abs(x) + Math.abs(y);
-            steps++;
+                md = Math.abs(x) + Math.abs(y);
+                steps++;
+            }
+            sum=sum+steps;
         }
-        sum=sum+steps;
-    }
         double average=(sum/trials);
         System.out.println("average number of steps = " + average);
     }
